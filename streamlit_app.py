c010985932747d9ca442edcf9435d1e02389dd47
Matheus Alves <49885908+MatheussAlvess@users.noreply.github.com>
2024-02-29 19:31:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,16 @@ if 'image_labels' not in st.session_state:
 if 'crop_button' not in st.session_state:
     st.session_state.crop_button = False
 
-def saving_increment(label,name,img):
+def saving_increment(label, name, img):
     ## Incrementa o contador para obter a próxima foto
     st.session_state.image_labels.append(label)
-    cv2.imwrite(f'images/{label}/{name}', img)
+    img.save(f'images/{label}/{name}')
     st.session_state.counter += 1
 
-def saving_increment_crop(label,name,img):
+def saving_increment_crop(label, name, img):
     ## Incrementa o contador para obter a próxima foto
     st.session_state.image_labels.append(label)
-    img = img.save(f'images/{label}/{name}')
-    
+    img.save(f'images/{label}/{name}')
     st.session_state.counter += 1
     st.session_state.crop_button = False
 
@@ -78,9 +77,7 @@ def main():
                 if not realtime_update:
                     st.write("Double click to save crop")
                 # Get a cropped image from the frontend
-                cropped_img = st_cropper(img, realtime_update=realtime_update, box_color='yellow',
-                                         )
-                # preview = st_cropper(img, realtime_update=realtime_update, box_color='yellow')
+                cropped_img = st_cropper(img, realtime_update=realtime_update, box_color='yellow')
                 
                 st.write("Preview")
                 _ = cropped_img.thumbnail((500,500))
@@ -89,33 +86,27 @@ def main():
                 st.subheader('Choose a label for the image')
                 label = st.radio(f"label:",[labels[i] for i in range(len(labels))])
 
-                st.button(f"Next image",on_click=saving_increment_crop,args=[label,images[st.session_state.counter].name,cropped_img])
+                st.button(f"Next image", on_click=saving_increment_crop, args=[label, images[st.session_state.counter].name, cropped_img])
                 
-                # my_bar = st.progress((st.session_state.counter/len(images)), text=progress_text)
                 st.progress((st.session_state.counter/len(images)), text=progress_text)
            
             except:
-                st.success("All images has been labeled successfully!")
-                # st.progress((st.session_state.counter/len(images)), text=progress_text)
+                st.success("All images have been labeled successfully!")
                 st.balloons()
         else:
             try:
-                image_data = cv2.imdecode(np.frombuffer(images[st.session_state.counter].read(), np.uint8), 1)
-                image_print = cv2.cvtColor(np.array(image_data), cv2.COLOR_BGR2RGB)
-                st.image(image_print, caption=f"Image {st.session_state.counter+1}")
+                img = Image.open(images[st.session_state.counter])
+                st.image(img, caption=f"Image {st.session_state.counter+1}")
 
                 st.subheader('Choose a label for the image')
                 label = st.radio(f"label:",[labels[i] for i in range(len(labels))])
 
-                st.button(f"Next image",on_click=saving_increment,args=[label,images[st.session_state.counter].name,image_data])
+                st.button(f"Next image", on_click=saving_increment, args=[label, images[st.session_state.counter].name, img])
                 st.progress((st.session_state.counter/len(images)), text=progress_text)
 
             except:
-                st.success("All images has been labeled successfully!")
-                # st.progress((st.session_state.counter/len(images)), text=progress_text)
+                st.success("All images have been labeled successfully!")
                 st.balloons()
 
-                
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
-
